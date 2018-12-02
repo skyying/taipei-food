c5c6717f254dae7c5baf1e4c5ff8b3be9c3f0e0a
Chien Wen Liu <dreamuk@gmail.com>
2018-12-02 23:18:03 +0800

    add comments

--- src/index.js
+++ src/index.js
@@ -13,108 +13,123 @@ import {getPriceState} from "./logic/getPriceState.js";
 import {isMatchKeyword} from "./logic/filterKeyword.js";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            keyword: "",
-            isFirstToggle: true,
-            price: {$: true, $$: true, $$$: true, $$$$: true},
-            isOpenNow: false
-        };
-        this.updateKeyword = this.updateKeyword.bind(this);
-        this.updatePrice = this.updatePrice.bind(this);
-        this.updateOpenTime = this.updateOpenTime.bind(this);
-    }
-    componentDidMount() {
-        const itemListData = jsonData.data.search.business.sort(
-            (a, b) => b.rating - a.rating
-        );
-        this.setState({list: itemListData});
-    }
-    updateKeyword(keyword) {
-        this.setState({keyword: keyword});
-    }
-    updatePrice(opt) {
-        this.setState(prevState => getPriceState(prevState, opt));
-    }
-    updateOpenTime() {
-        this.setState(prevState => {
-            return {isOpenNow: !prevState.isOpenNow};
-        });
-    }
-    render() {
-        let list = this.state.list || [];
+  constructor(props) {
+    super(props);
 
-        if (list.length) {
-            list = list.filter((item, index) => {
-                return (
-                    this.state.price[item.price] ||
-                    (this.state.isFirstToggle && item.price === null)
-                );
-            });
-        }
+    this.state = {
+      // user input to filter search results
+      keyword: "",
 
-        if (this.state.isOpenNow) {
-            list = list.filter(item =>
-                isRestaurantOpenNow(item.hours, item.name)
-            );
-        }
+      // use to detect if all price tag haven't been selected yet
+      isFirstToggle: true,
 
-        if (this.state.keyword) {
-            list = list.filter(item =>
-                isMatchKeyword(item, this.state.keyword.toLowerCase())
-            );
-        }
+      // price tag to filter results
+      price: {$: true, $$: true, $$$: true, $$$$: true},
 
+      // is open now filter button selected
+      isOpenNow: false
+    };
+
+    ["updateKeyword", "updatePrice", "updatePrice"].forEach(name => {
+      this[name] = this[name].bind(this);
+    });
+  }
+  componentDidMount() {
+    // fetch json files, and sort fetched result by rating
+    const itemListData = jsonData.data.search.business.sort(
+      (itemA, itemB) => itemA.rating - itemB.rating
+    );
+
+    this.setState({list: itemListData});
+  }
+  updateKeyword(keyword) {
+    this.setState({keyword: keyword});
+  }
+  updatePrice(opt) {
+    this.setState(prevState => getPriceState(prevState, opt));
+  }
+  updateOpenTime() {
+    this.setState(prevState => {
+      return {isOpenNow: !prevState.isOpenNow};
+    });
+  }
+
+  render() {
+    let list = this.state.list || [];
+
+    // filter results by price filter
+    if (list.length) {
+      list = list.filter((item, index) => {
         return (
-            <div>
-                <header>
-                    <div>
-                        <div className="search-row">
-                            <input
-                                value={this.state.keyword}
-                                placeholder="Find a restanrant"
-                                onChange={e =>
-                                    this.setState({
-                                        keyword: e.currentTarget.value
-                                    })
-                                }
-                            />
-                            <div className="location">
-                                <span>Near Taipei</span>
-                            </div>
-                        </div>
-                        <Category
-                            updateKeyword={this.updateKeyword}
-                            options={[
-                                "Hot Pot",
-                                "Seafood",
-                                "Desserts",
-                                "Brunch",
-                                "Taiwanese"
-                            ]}
-                        />
-                    </div>
-                </header>
-                <main>
-                    <div>
-                        <ResultTitle count={list.length} />
-                        <FilterOptions
-                            isFirstToggle={this.state.isFirstToggle}
-                            price={this.state.price}
-                            isOpenNow={this.state.isOpenNow}
-                            updatePrice={this.updatePrice}
-                            updateOpenTime={this.updateOpenTime}
-                        />
-                        <ResultList
-                            updateKeyword={this.updateKeyword}
-                            list={list}
-                        />
-                    </div>
-                </main>
-            </div>
+          this.state.price[item.price] ||
+                    (this.state.isFirstToggle && item.price === null)
         );
+      });
     }
+
+    // filter results by if restaurants is open now.
+    if (this.state.isOpenNow) {
+      list = list.filter(item =>
+        isRestaurantOpenNow(item.hours, item.name)
+      );
+    }
+
+    // filter results by if a user inputs any keywowrd
+    if (this.state.keyword) {
+      list = list.filter(item =>
+        isMatchKeyword(item, this.state.keyword.toLowerCase())
+      );
+    }
+
+    return (
+      <div>
+        <header>
+          <div>
+            <div className="search-row">
+              <input
+                value={this.state.keyword}
+                placeholder="Find a restanrant"
+                onChange={e =>
+                  this.setState({
+                    keyword: e.currentTarget.value
+                  })
+                }
+              />
+              <div className="location">
+                <span>Near Taipei</span>
+              </div>
+            </div>
+            <Category
+              updateKeyword={this.updateKeyword}
+              options={[
+                "Hot Pot",
+                "Seafood",
+                "Desserts",
+                "Brunch",
+                "Taiwanese"
+              ]}
+            />
+          </div>
+        </header>
+        <main>
+          <div>
+            <ResultTitle count={list.length} />
+            <FilterOptions
+              isFirstToggle={this.state.isFirstToggle}
+              price={this.state.price}
+              isOpenNow={this.state.isOpenNow}
+              updatePrice={this.updatePrice}
+              updateOpenTime={this.updateOpenTime}
+            />
+            <ResultList
+              updateKeyword={this.updateKeyword}
+              list={list}
+            />
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("main"));
